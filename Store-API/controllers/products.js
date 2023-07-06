f027ba6f7ff68c1d780168d8eb164b00591dfc5c
Phan Anh <phananh12042003@gmail.com>
2023-07-06 18:49:29 -0400

    pagination 🚀

--- Store-API/controllers/products.js
+++ Store-API/controllers/products.js
@@ -3,12 +3,14 @@ const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
   const search = "a";
-  const products = await Product.find({}).sort("name");
+  const products = await Product.find({}).sort("name").select("name price");
+
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  console.log(req.query);
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -22,12 +24,27 @@ const getAllProducts = async (req, res) => {
   }
   //get all products
   let result = Product.find(queryObject);
+  //sort
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
     result = result.sort("createdAt");
   }
+
+  //fields
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  const page = Number(req.query.page) || 1; //if user does not pass any value
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
