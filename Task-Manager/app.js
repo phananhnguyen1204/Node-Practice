387a6631247f1fb346a8ed3e1decacc5a58d3b60
Phan Anh <phananh12042003@gmail.com>
2023-07-05 21:30:16 -0400

    env 🚀

--- Task-Manager/app.js
+++ Task-Manager/app.js
@@ -1,7 +1,8 @@
-require("./db/connect");
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 // MIDDLEWARE
 app.use(express.json());
@@ -15,7 +16,15 @@ app.get("/hello", (req, res) => {
 app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`server is listening on port ${port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-app.listen(port, () => {
-  console.log(`server is listening on port ${port}`);
-});
+start();
--- Task-Manager/db/connect.js
+++ Task-Manager/db/connect.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://phananh:phananhjackjack@task-manager-api.i4fqviy.mongodb.net/Task-Manager?retryWrites=true&w=majority";
-
-mongoose
-  .connect(connectionString, {
+const connectDB = (url) => {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-  .then(() => console.log("CONNECTED TO THE DB"))
-  .catch((err) => console.log(e.message));
+  });
+};
+
+module.exports = connectDB;
